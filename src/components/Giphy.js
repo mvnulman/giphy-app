c97ad3cefb1f7e474de808b4d050c7f98c4a2c16
Marcos Vinicius <mvsouza7@gmail.com>
2022-01-11 20:56:23 +0200

    docs: added the sweet alert2 dependency to the project and the search form field! now you can search for your gifs and be warned if the search field is empty

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -3,11 +3,15 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "../App.css";
 import Spinner from "react-bootstrap/Spinner";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const Giphy = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [search, setSearch] = useState("");
+  const MySwal = withReactContent(Swal);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,8 +30,7 @@ const Giphy = () => {
         setData(results.data.data);
       } catch (error) {
         setIsError(true);
-        setTimeout(() => setIsError(false),
-          3000);
+        setTimeout(() => setIsError(false), 3000);
       }
 
       setIsLoading(false);
@@ -57,16 +60,68 @@ const Giphy = () => {
 
   const renderError = () => {
     if (isError) {
-      return <div className="alert alert-danger alert-dismissible.fade.show" role="alert">
-      Unable to get Gifs. Please try again in a few minutes.
-      {/* <button className="close">Close</button> */}
-    </div>;
+      return (
+        <div
+          className="alert alert-danger alert-dismissible.fade.show"
+          role="alert"
+        >
+          Unable to get Gifs. Please try again in a few minutes.
+        </div>
+      );
     }
-  }
+  };
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsError(false);
+    setTimeout(() => setIsError(false), 1500);
+    setIsLoading(true);
+
+    if (search.length === 0) {
+      MySwal.fire({
+        icon: "error",
+        title: "Oops... ",
+        text: "Your search field is empty!",
+      });
+      setIsLoading("");
+      return;
+    }
+
+    const searchResults = await axios("https://api.giphy.com/v1/gifs/search", {
+      params: {
+        api_key: "6FWRNXMBWZPPWKYVnN9kcDAd9xjiue1x",
+        q: search,
+      },
+    });
+    setData(searchResults.data.data);
+    setIsLoading(false);
+  };
 
   return (
     <div className="m-2">
       {renderError()}
+      <div className="d-flex justify-content-center">
+        <form>
+          <input
+            value={search}
+            onChange={handleSearchChange}
+            type="text"
+            placeholder="Search here..."
+            // className="form-control"
+          />
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="btn btn-primary ms-2"
+          >
+            Search!
+          </button>
+        </form>
+      </div>
       <div className="container">{renderGifs()}</div>
     </div>
   );
