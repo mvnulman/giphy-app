9eac074b64f680c7eb1f55d3824c0b9516bcd25d
Marcos Vinicius <mvsouza7@gmail.com>
2025-11-20 01:52:00 -0300

    Chore: Refactor code structure for improved readability and maintainability

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+const root = createRoot(rootElement);
+root.render(<App />);
